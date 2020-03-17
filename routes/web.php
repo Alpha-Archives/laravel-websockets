da6b07ce42fce4342cafc5b27095eed93cf861b3
Alpha Olomi <alphaolomi@gmail.com>
2020-03-17 19:32:56 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', 'ChatsController@index');
+    Route::get('/', 'ChatsController@index')->name('chats.index');
     Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('messages', 'ChatsController@fetchMessages');
-    Route::post('messages', 'ChatsController@sendMessage');
+    Route::get('messages', 'ChatsController@fetchMessages')->name('chats.fetch');
+    Route::post('messages', 'ChatsController@sendMessage')->name('chats.send');
 });
