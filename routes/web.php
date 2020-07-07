17e869639c3de91cd690c4379807d45feaf354c6
Alpha Olomi <alphaolomi@gmail.com>
2020-07-07 11:30:28 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,34 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::get('messages', 'ChatsController@fetchMessages')->name('chats.fetch');
     Route::post('messages', 'ChatsController@sendMessage')->name('chats.send');
+
+
+    Route::get('job', 'ReportController@generate')->name('reports.generate');
+
+
+
+
+    Route::get('test', function () {
+        event(new App\Events\StatusLiked('Someone'));
+        return "Event has been sent!";
+    });
+    
+    
+    Route::get('noti', function () {
+        $user = auth()->user();
+        $users  = User::all();
+        $user->notify(new App\Notifications\InvoicePaid());
+        // Notification::send($users, new App\Notifications\InvoicePaid());
+        return "Notified!";
+    });
+
+
+
 });
 
 
 
-Route::get('test', function () {
-    event(new App\Events\StatusLiked('Someone'));
-    return "Event has been sent!";
-});
-
-
-Route::get('noti', function () {
-    $user = auth()->user();
-    $users  = User::all();
-    $user->notify(new App\Notifications\InvoicePaid());
-    // Notification::send($users, new App\Notifications\InvoicePaid());
-    return "Notified!";
-});
 
 
 
-Route::get('job', 'ReportController@generate')->name('reports.generate');
+
