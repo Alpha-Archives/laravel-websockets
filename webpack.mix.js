79558eff37aafb9851e241f4c342e8b23ff3a349
Alpha Olomi <alphaolomi@gmail.com>
2020-12-30 10:51:29 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,11 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+   .sass('resources/sass/app.scss', 'public/css');
+
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
